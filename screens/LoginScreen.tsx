61d4cf2971cb11183bda1a6dd9400fc155997ef0
minhoangg0712 <suareznguyen7@gmail.com>
2025-08-03 00:43:08 +0700

    Sửa giao diện đăng nhập

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -105,7 +105,7 @@ const LoginScreen: React.FC = () => {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.header}>
-        <TouchableOpacity onPress={() => navigation.goBack()}>
+        <TouchableOpacity onPress={() => navigation.replace('Home')}>
           <Icon name="close-circle-outline" size={40} color="white" style={styles.headerIcon} />
         </TouchableOpacity>
       </View>
